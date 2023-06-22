6e940d01b9b776924557d28b4919f62728017136
0D1NSS0N <115773776+0d1nss0n@users.noreply.github.com>
2023-06-22 16:00:59 -0700

    Create spookie.ps1

---
+++ Spookie-Movie/spookie.ps1
@@ -0,0 +1,16 @@
+﻿# Set the target time (11:59 PM)
+$targetTime = Get-Date -Hour 23 -Minute 59 -Second 0
+
+# Calculate the time to wait in seconds
+$waitTime = ($targetTime - (Get-Date)).TotalSeconds
+
+# Wait until the target time
+Start-Sleep -Seconds $waitTime
+
+# Set the volume to 50%
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+
+# Start Chrome in fullscreen
+$chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$chromeArgs = "--start-fullscreen https://pluto.tv/en/on-demand/movies/61e9f621afe81f001a25b4d2"
+Start-Process -FilePath $chromePath -ArgumentList $chromeArgs
